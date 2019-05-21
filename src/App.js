8dbd38694845d28377dadbb779aff4a0558ad320
robrze <robert.rzepnikowski@o2.pl>
2019-05-21 13:13:16 +0200

    searching from list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">App</div>;
+class App extends Component {
+  state = {
+    listArray: ["element 21", "element 22", "element 3"],
+    value: ""
+  };
+
+  renderList = () => {
+    const { listArray, value } = this.state;
+
+    return listArray
+      .filter(element => element.includes(this.state.value))
+      .map(element => (
+        <p className={value ? "Green" : "black"} key={element}>
+          {element}
+        </p>
+      ));
+  };
+
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <label>
+          search:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <div className="App">{this.renderList()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
